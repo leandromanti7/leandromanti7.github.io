37e89b79198cf8393eda555e26df25ce7e6f4127
leandromanti7 <manti.leandro@gmail.com>
2025-06-23 23:21:17 +0200

    Update input.js

--- vr-robot/controllers/input.js
+++ vr-robot/controllers/input.js
@@ -1,5 +1,5 @@
 // controllers/input.js
-import * as THREE from './three.module.js';
+import * as THREE from '../three.module.js';
 
 export function setupControllers(scene, renderer) {
   const controller1 = renderer.xr.getController(0);
